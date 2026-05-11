82cb6ba5563536979db908a66def090424800d10
ilian-marchitto <ilian.marchitto@etu.univ-amu.fr>
2026-05-11 17:24:12 +0200

    ajout des requetes de recherche par Username ou Email

--- api/models/Utilisateur.php
+++ api/models/Utilisateur.php
@@ -2,9 +2,27 @@
 
 namespace Api\Models;
 
+Use PDO;
+
 class UtilisateurModel extends Model {
     protected $table = 'utilisateurs';
     protected $primaryKey = 'id_utilisateur';
+
+    public function findByUsername(string $identifiant) {
+        $sql = "SELECT * FROM " . $this->table . " WHERE identifiant = :identifiant";
+
+        $query = $this->db->prepare($sql);
+        $query->execute(['identifiant' => $identifiant]);
+        return $query->fetch(PDO::FETCH_ASSOC);
+    }
+
+    public function findByEmail(string $email) {
+        $sql = "SELECT * FROM " . $this->table . " WHERE email = :email";
+
+        $query = $this->db->prepare($sql);
+        $query->execute(['email' => $email]);
+        return $query->fetch(PDO::FETCH_ASSOC);
+    }
 }
 
 ?>
